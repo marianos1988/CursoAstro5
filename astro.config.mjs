95a17206eacb9fc6dbbf4f5719e648062c6c250a
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2025-06-18 11:18:28 -0300

    Agregar server Island

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig, envField } from 'astro/config'; // El envField define el 
 
 // https://astro.build/config
 export default defineConfig({
+    output: "server",
     env: {
         schema: {
             SHOW_BUY_BUTTON: envField.boolean({ context: "server", access: "public"}),
-            SCORE_API_ENDPOINT:envField.string({ context: "client", access: "public"})
+            SCORE_API_ENDPOINT:envField.string({ context: "server", access: "public"})
         }
     }
 });
